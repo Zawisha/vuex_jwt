fc93bce51f08808b841e8b4fc7e7e85b03addf3f
Zawisha <ogniemimechem@gmail.com>
2019-07-13 12:41:12 +0300

    add vuex & jwtAuth

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+       I'M HEADER
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm HOME COMPONENT and {{ welcome }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        computed: {
+            welcome() {
+                return this.$store.getters.welcome            }
+        }
+    }
+</script>
